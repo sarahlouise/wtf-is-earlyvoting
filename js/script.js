bc62c87021c92755ba23de3572443a44071b6905
Sarah Kruberg <sarah.kruberg@gmail.com>
2016-09-21 16:38:59 -0400

    smooth scroll now working

--- js/script.js
+++ js/script.js
@@ -136,35 +136,20 @@
                 }
             });
 
-            // Add scrollspy to <body>
-            $('body').scrollspy({
-                target: ".navbar",
-                offset: 50
-            });
-
             //Smoothscroll
             $('a').smoothScroll();
 
+            function scrollNav() {
+              $('.nav a').click(function(){  
 
-            
-            $("#AutoScrollNav a").on('click', function(event) {
-                // Make sure this.hash has a value before overriding default behavior
-                if (this.hash !== "") {
-                    // Prevent default anchor click behavior
-                    event.preventDefault();
+                //Animate
+                $('html, body').stop().animate({
+                    scrollTop: $( $(this).attr('href') ).offset().top - 70
+                }, 400);
+                return false;
+              });
+              $('.scrollTop a').scrollTop();
+            }
+            scrollNav();
 
-                    // Store hash
-                    var hash = this.hash;
-
-                    // Using jQuery's animate() method to add smooth page scroll
-                    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-                    $('html, body').animate({
-                        scrollTop: $(hash).offset().top
-                    }, 800, function() {
-
-                        // Add hash (#) to URL when done scrolling (default click behavior)
-                        window.location.hash = hash;
-                    });
-                }
-            })
         })
